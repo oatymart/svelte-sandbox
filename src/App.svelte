7f691951d1ca553b4b3aabcaf4ddc9d12bea6cde
Martin Nicholson <martin@taotesting.com>
2019-11-06 15:11:25 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let name;
+</script>
+
+<style lang="scss">
+	:root {
+		--the-color-purple: purple;
+	}
+
+	@import './scss/scss_vars.scss';
+
+	@import './scss/css_custom_props.scss';
+
+	h1 {
+		$theColorPurple: purple !default;
+
+		color: var(--the-color-purple);
+		color: $theColorPurple;
+	}
+</style>
+
+<div>
+	<h1>Hello {name}!</h1>
+</div>
